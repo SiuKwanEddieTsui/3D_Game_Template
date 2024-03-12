79492bfc3b6ed55f2b05dd9edfc8f209768be925
SiuKwanEddieTsui <158217513+SiuKwanEddieTsui@users.noreply.github.com>
2024-03-12 17:39:27 -0400

    PickupScriptUpdated

--- Scenes/PickupArea2.gd
+++ Scenes/PickupArea2.gd
@@ -10,39 +10,36 @@ var pickup_object = null
 var is_picked = false
 
 func _physics_process(_delta):
-	#check that we have an item picked up
+	# Check that we have an item picked up
 	if pickup_object and is_picked:
-		# get the distance between object and hand
+		# Get the distance between object and hand
 		var obj_pos = pickup_object.global_transform.origin
 		var hand_pos = hand.global_transform.origin
-		# get the distance beteween object and hand
+		# Calculate the direction and apply speed
 		var dir = (hand_pos - obj_pos).normalized() * pull_speed
-		#move the object in that direction
+		# Move the object in that direction
 		pickup_object.set_linear_velocity(dir)
 
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("pickup"):
-		#if user hits "f" or pickup button
+		# If user hits "f" or pickup button
 		if pickup_object:
-			# if object already picked up
 			if is_picked:
-				# drop object
+				# Drop object
+				pickup_object.set_linear_velocity(Vector3.ZERO) # Stop moving the object when dropped
 				pickup_object = null
 				is_picked = false
 			else:
 				is_picked = true
-				
-	if Input.is_action_just_pressed("throw"):
+
+	if Input.is_action_just_pressed("throw") and is_picked:
 		is_picked = false
-		var dir = -camera.global_transform.basis.z.normalized()
-		pickup_object.apply_impulse(dir)
+		var dir = -camera.global_transform.basis.z.normalized() * throw_speed
+		pickup_object.apply_impulse(Vector3.ZERO, dir) # Correct use of apply_impulse
 		pickup_object = null
-		
 
 func _on_body_entered(body):
-	# first check if an object is picked up
-	if pickup_object and is_picked:
-		# if so, end function here
-		return
+	# First check if an object is picked up
+	if not pickup_object or not is_picked:
 		pickup_object = body
-		print(pickup_object)
+		print("Picked up: ", pickup_object)
