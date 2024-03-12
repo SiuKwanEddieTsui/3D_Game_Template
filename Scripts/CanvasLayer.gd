eac0c36ba0a2aaa7ef27908f999d79a2f26104b7
SiuKwanEddieTsui <158217513+SiuKwanEddieTsui@users.noreply.github.com>
2024-03-12 11:56:59 -0400

    Added Collectibles

---
+++ Scripts/CanvasLayer.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+func update_collected_amount(amount):
+	$CollectedLabel.text = "Coins: " + str(amount)
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
