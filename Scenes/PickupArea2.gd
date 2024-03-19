2e441913decfb2177ebe349ebbf0cebb7c470286
SiuKwanEddieTsui <158217513+SiuKwanEddieTsui@users.noreply.github.com>
2024-03-19 06:25:45 -0400

    Physics and Pickup/throw actions added

--- Scenes/PickupArea2.gd
+++ Scenes/PickupArea2.gd
@@ -1,45 +1,35 @@
 extends Area3D
 
-@onready var hand = $"../hand"
-@onready var camera = $".."
+@onready var hand = $"../hand"  # Make sure this matches your scene structure
+@onready var camera = $".."  # Make sure this matches your scene structure
 
-var pull_speed = 2
-var throw_speed = 8
-
-var pickup_object = null
+var picked_object : RigidBody3D = null
 var is_picked = false
 
-func _physics_process(_delta):
-	# Check that we have an item picked up
-	if pickup_object and is_picked:
-		# Get the distance between object and hand
-		var obj_pos = pickup_object.global_transform.origin
-		var hand_pos = hand.global_transform.origin
-		# Calculate the direction and apply speed
-		var dir = (hand_pos - obj_pos).normalized() * pull_speed
-		# Move the object in that direction
-		pickup_object.set_linear_velocity(dir)
+const PULL_SPEED = 4.0
+const THROW_FORCE = 10.0
+const UPWARD_THROW_ADDITION = Vector3(0, 1, 0)  # Adds a natural upward trajectory
 
-func _unhandled_input(event):
-	if Input.is_action_just_pressed("pickup"):
-		# If user hits "f" or pickup button
-		if pickup_object:
-			if is_picked:
-				# Drop object
-				pickup_object.set_linear_velocity(Vector3.ZERO) # Stop moving the object when dropped
-				pickup_object = null
-				is_picked = false
-			else:
-				is_picked = true
+func _physics_process(_delta: float) -> void:
+	if picked_object and is_picked:
+		var object_position = picked_object.global_transform.origin
+		var hand_position = hand.global_transform.origin
+		picked_object.linear_velocity = (hand_position - object_position) * PULL_SPEED
 
-	if Input.is_action_just_pressed("throw") and is_picked:
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("picked") and picked_object:
+		is_picked = not is_picked
+		if not is_picked and picked_object:
+			picked_object.linear_velocity = Vector3.ZERO
+			picked_object.angular_velocity = Vector3.ZERO
+			picked_object = null
+
+	if event.is_action_pressed("throw") and is_picked and picked_object:
+		var dir = -camera.global_transform.basis.z.normalized() + UPWARD_THROW_ADDITION
+		picked_object.apply_central_impulse(dir * THROW_FORCE)
+		picked_object = null
 		is_picked = false
-		var dir = -camera.global_transform.basis.z.normalized() * throw_speed
-		pickup_object.apply_impulse(Vector3.ZERO, dir) # Correct use of apply_impulse
-		pickup_object = null
 
-func _on_body_entered(body):
-	# First check if an object is picked up
-	if not pickup_object or not is_picked:
-		pickup_object = body
-		print("Picked up: ", pickup_object)
+func _on_body_entered(body: Node) -> void:
+	if not is_picked and body is RigidBody3D:
+		picked_object = body
--- Scripts/Pickup.gd
+++ Scripts/Pickup.gd
@@ -2,47 +2,31 @@ extends Area3D
 
 @onready var hand = $"../hand"
 @onready var camera = $".."
-
-var pull_speed = 2
-var throw_speed = 8
-
-var pickup_object = null
+var picked_object = null
 var is_picked = false
 
 func _physics_process(_delta):
-	#check that we have an item picked up
-	if pickup_object and is_picked:
-		# get the distance between object and hand
-		var obj_pos = pickup_object.global_transform.origin
-		var hand_pos = hand.global_transform.origin
-		# get the distance beteween object and hand
-		var dir = (hand_pos - obj_pos).normalized() * pull_speed
-		#move the object in that direction
-		pickup_object.set_linear_velocity(dir)
+	if picked_object and is_picked:
+		var a = picked_object.global_transform.origin
+		var b = hand.global_transform.origin
+		picked_object.set_linear_velocity((b - a) * 4)
 
-func _unhandled_input(event):
-	if Input.is_action_just_pressed("pickup"):
-		#if user hits "f" or pickup button
-		if pickup_object:
-			# if object already picked up
-			if is_picked:
-				# drop object
-				pickup_object = null
-				is_picked = false
-			else:
-				is_picked = true
-				
-	if Input.is_action_just_pressed("throw"):
+func _unhandled_input(_event):
+	if (Input.is_action_just_pressed("picked")) and picked_object:
+		if is_picked:
+			picked_object = null
+			is_picked = false
+		else:
+			is_picked = true
+	
+	if (Input.is_action_just_pressed("throw")) and picked_object and is_picked:
+		var dir = -camera.global_transform.basis.z.normalized() + Vector3(0, 1, 0)
+		picked_object.apply_central_impulse(dir * 10)
+		picked_object = null
 		is_picked = false
-		var dir = -camera.global_transform.basis.z.normalized()
-		pickup_object.apply_impulse(dir)
-		pickup_object = null
-		
 
 func _on_body_entered(body):
-	# first check if an object is picked up
-	if pickup_object and is_picked:
-		# if so, end function here
+	if picked_object and is_picked:
 		return
-		pickup_object = body
-		print(pickup_object)
+	picked_object = body
+	
