92bcbfba03e53465136a19cd4b6774fea6757feb
SiuKwanEddieTsui <158217513+SiuKwanEddieTsui@users.noreply.github.com>
2024-03-26 15:48:39 -0400

    Fixed Navmap bug

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -2,8 +2,12 @@ extends CharacterBody3D
 
 @onready var nav_agent = $NavigationAgent3D
 @export var speed = 3
+var is_nav_loaded : bool = false
 
 func _physics_process(delta):
+	if not is_nav_loaded:
+		is_nav_loaded = true
+		return
 	var current_location = global_transform.origin
 	var next_location = nav_agent.get_next_path_position()
 	var new_velocity = (next_location - current_location).normalized() * speed
