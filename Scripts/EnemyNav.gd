aa5296e8f0be4800c41270bc4a9bceb1bc651487
SiuKwanEddieTsui <158217513+SiuKwanEddieTsui@users.noreply.github.com>
2024-05-14 12:31:49 -0400

    Update EnemyNav.gd

--- Scripts/EnemyNav.gd
+++ Scripts/EnemyNav.gd
@@ -1,11 +1,5 @@
 extends Node3D
-
 @onready var player = $"../Player"
 
 func _physics_process(_delta):
-	# Check if the player node exists
-	if player:
-		# Check if there are enemies in the current level
-		var enemies = get_tree().get_nodes_in_group("enemies")
-		if enemies.size() > 0:
-			get_tree().call_group("enemies", "update_target_location", player.global_transform.origin)
+	get_tree().call_group("enemies", "update_target_location", player.global_transform.origin)
